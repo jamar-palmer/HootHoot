02eadf7281db09c698b3dececaf4f7dd09b601a2
BrandonDorjkh <62077282+BrandonDorjkh@users.noreply.github.com>
2021-04-22 21:08:17 -0400

    Add files via upload

---
+++ HootHootLibrary/Course.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HootHootLibrary
+{
+    class Course
+    {
+
+        public string subject { get; set; }
+        public string semester { get; set; }
+        public string college { get; set; }
+        public string campus { get; set; }
+        public string instructor { get; set; }
+        public int crn { get; set; }
+        public int section { get; set; }
+        public double credithours { get; set; }
+        public DateTime meetingtime { get; set; }
+        public int capacity { get; set; }
+
+        public Course (string s, string sm, string co, string ca, string ins, int cr, int se, double ch, DateTime mt, int cap)
+        {
+            subject = s;
+            semester = sm;
+            college = co;
+            campus = ca;
+            instructor = ins;
+            crn = cr;
+            section = se;
+            credithours = ch;
+            meetingtime = mt;
+            capacity = cap;
+
+
+        }
+
+
+
+    }
+}
---
+++ HootHootLibrary/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HootHootLibrary
+{
+    class User
+    {
+        public string username { get; set; }
+        private string password { get; set; }
+        public string usertype { get; set; }
+
+        public User (string un, string pw, string ut)
+        {
+            username = un;
+            password = pw;
+            usertype = ut;
+        }
+    }
+}
